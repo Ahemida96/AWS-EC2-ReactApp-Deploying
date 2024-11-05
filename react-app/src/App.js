d6511cba17a1d89fbe755cfa831c149976d925f4
Ahmed Hemida <ahmedhemida121@gmail.com>
2024-11-05 18:11:40 +0200

    Submitting the sample react-app

---
+++ react-app/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import React from 'react';
+import WelcomePage from 'C:\\Users\\Ahmed\\Desktop\\AWS-EC2-ReactApp-Deploying\\react-app\\src\\components\\WelcomePage.tsx';
+// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+const App = () => {
+  return (
+    // <Router>
+    //   <Routes>
+    //     <Route exact path="/" component={WelcomePage} />
+    //     {/* Add other routes here */}
+    //   </Routes>
+    // </Router>
+    <WelcomePage />
+  );
+};
+
+export default App;
