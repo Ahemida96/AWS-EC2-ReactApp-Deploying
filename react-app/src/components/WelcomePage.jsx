6fb1601d6eb013cab80e628f062c69f85c7a269f
Ahmed Hemida <ahmedhemida121@gmail.com>
2024-11-07 16:55:56 +0200

    Update the welcome page to test the CI/CD workflow

--- react-app/src/components/WelcomePage.jsx
+++ react-app/src/components/WelcomePage.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 const WelcomePage = () => {
   return (
     <div className="welcome-page">
-      <h1>Welcome to Our App</h1>
+      <h1>Welcome to CI/CD Automated App</h1>
       <p>This is a sample welcome page.</p>
     </div>
   );
